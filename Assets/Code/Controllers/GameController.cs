fe5f32d9793f308f773386a51f4153e0e5b831f9
Pavel4096 <79263605+Pavel4096@users.noreply.github.com>
2021-08-16 18:57:43 +0300

    Lesson9

--- Assets/Code/Controllers/GameController.cs
+++ Assets/Code/Controllers/GameController.cs
@@ -123,5 +123,4 @@ namespace Car
                 xmlSerializer.Serialize(outputStream, _userData);
             }
         }
-    }
 }
---
+++ Assets/Code/Editor/BuildAssetBundles.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class BuildAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    private static void StartAssetBundlesBuild()
+    {
+        const string assetbundlesfolder = "Assets/AssetBundles";
+
+        if(!Directory.Exists(assetbundlesfolder))
+            Directory.CreateDirectory(assetbundlesfolder);
+
+        BuildPipeline.BuildAssetBundles(assetbundlesfolder, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+    }
+}
